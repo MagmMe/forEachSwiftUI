233021e9a7c9a82176567f66f2d2d929272036ce
Marcin Magiera <marcin@magm.me>
2021-07-30 09:34:58 +0200

    Initial Commit

---
+++ ForEach in View/ForEach_in_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForEach_in_ViewApp.swift
+//  ForEach in View
+//
+//  Created by Magme on 30/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ForEach_in_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
